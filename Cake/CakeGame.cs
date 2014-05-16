e9c85338e832976bb3550ed2aeab8416ede7220e
Koen Bollen <meneer@koenbollen.nl>
2014-05-16 11:13:14 +0200

    Makes Windows build work and adds content.

--- Cake/CakeGame.cs
+++ Cake/CakeGame.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using Phantom;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Cake
 {
 	public class CakeGame : PhantomGame
 	{
+        private Texture2D white;
+        private SpriteBatch batch;
 		public CakeGame ()
 			:base(1280, 720, "Cake")
 		{
 		}
 
+        protected override void Initialize()
+        {
+            XnaGame.IsMouseVisible = true;
+            white = Content.Load<Texture2D>("sprites/white");
+            batch = new SpriteBatch(this.GraphicsDevice);
+            base.Initialize();
+        }
+
+        public override void Render(Phantom.Graphics.RenderInfo info)
+        {
+            batch.Begin();
+            batch.Draw(white, new Rectangle(10, 10, 100, 100), Color.White);
+            batch.Draw(white, new Rectangle(40, 50, 300, 100), Color.Black);
+            batch.End();
+            base.Render(info);
+        }
 	}
 }
 
---
+++ Cake/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+#if PLATFORM_WINDOWS
+
+#elif PLATFORM_MACOS
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+#elif PLATFORM_IOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
+
+
+namespace Cake
+{
+
+#if PLATFORM_WINDOWS
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (CakeGame game = new CakeGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#elif PLATFORM_MACOS
+	static class Program
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate ();
+				NSApplication.Main (args);
+			}
+		}
+	}
+
+	class AppDelegate : NSApplicationDelegate
+	{
+		CakeGame game;
+
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			game = new CakeGame ();
+			game.Run ();
+		}
+
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}
+#elif PLATFORM_IOS
+    [Register ("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		CakeGame game;
+
+		public override void FinishedLaunching (UIApplication app)
+		{
+			game = new CakeGame ();
+			game.Run ();
+		}
+
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, "UIApplication", "AppDelegate");
+		}
+	}
+#endif
+
+}
